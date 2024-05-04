4c82650e34ec643fe8641b56ee7cdc79b1aed068
Jeffrey Scoggins <JeffreyScoggins84@gmail.com>
2024-05-03 22:07:55 -0400

    connected EmailJS to website for functioning email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,8 @@ export default function Contact() {
     const form = useRef();
     const publicKey = process.env.REACT_APP_PUBLIC_KEY;
     const serviceID = process.env.REACT_APP_SERVICE_ID;
-    const templateID = process.env.REACT_APP_PUBLIC_KEY;
+    const templateID =process.env.REACT_APP_TEMPLATE_ID;
+    ;
 
 
 
@@ -31,30 +32,111 @@ export default function Contact() {
         },
     });
 
-    const sendEmail = (e) => {
+    const handleOnSubmit = (e) => {
         e.preventDefault();
-
-        emailjs
-            .send(serviceID, templateID, form.current)
-            .then(
-                () => {
-                    console.log('SUCCESS!');
-                },
-                (error) => {
-                    console.log('FAILED...', error.text);
-                },
-            );
+        emailjs.sendForm(serviceID, templateID, e.target, publicKey)
+            .then((result) => {
+                alert('Message Sent Successfully')
+            }, (error) => {
+                console.log(error.text);
+                alert('Something went wrong!')
+            });
+        e.target.reset()
     };
 
     return (
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
-        </form>
+        <section id="contact" className="relative">
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                <div className="lg:w-2/3 md:w-1/2 bg-gray-500 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        title="map"
+                        className="absolute inset-0"
+                        frameBorder={0}
+                        marginHeight={0}
+                        marginWidth={0}
+                        style={{ filter: "opacity(0.7)" }}
+                        src="https://www.google.com/maps/embed/v1/place?q=Duluth,+GA,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+                    />
+                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                        <div className="lg:w-1/2 px-6">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                LOCATION
+                            </h2>
+                            <p className="mt-1 text-white">
+                                Duluth, Georgia, United States <br />
+                            </p>
+                        </div>
+                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0 text-white">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                EMAIL
+                            </h2>
+                            <a className="text-white leading-relaxed">
+                                Jeffrey.Scoggins@me.com
+                            </a>
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                                PHONE:
+                            </h2>
+                            <p className="leading-relaxed">470-588-2466</p>
+                        </div>
+                    </div>
+                </div>
+                <form
+                    netlify
+                    name="contact"
+                    onSubmit={handleOnSubmit}
+                    className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        Hire Me
+                    </h2>
+                    <p className="leading-relaxed mb-5">
+                        <center>Please send me a message if you would love for me to work you or your company</center>
+                    </p>
+                    <div className="relative mb-4">
+                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="message"
+                            className="leading-7 text-sm text-gray-400">
+                            Message
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </section>
     );
 };
